181cd2f104ddd9e3d3fb65534d10e88a6b71ab98
praktikan <nayl.ara2300@gmail.com>
2026-08-27 08:59:00 +0700

    Change Linear Layout

--- app/src/main/java/com/example/pertemuan2_countapp/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan2_countapp/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.pertemuan2_countapp
 
+import android.R
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pertemuan2_countapp.databinding.ActivityMainBinding
+
+// bikin variabel untul binding
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //inisiasi binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //set content dari activity untuk menggunakan binding view root
+        setContentView(binding.root)
+
+        //sekarang kita bisa akses ui element di code bloc:
+        with(binding){
+
         }
     }
 }
